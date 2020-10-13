3517999966656322bb262e822b33fca174d60c87
Adriano Raiano <adriano@raiano.ch>
2020-10-13 18:00:59 +0200

    test cimode and exists

---
+++ test/cimode.spec.js
@@ -0,0 +1,56 @@
+import i18next from '../index.js'
+import should from 'should'
+
+describe('Translator', () => {
+  describe('translate() in cimode', () => {
+    let i18n
+
+    before(async () => {
+      i18n = i18next({ lng: 'cimode', fallbackLng: 'en' })
+      i18n.addHook('loadResources', () => ({
+        en: {
+          translation: {
+            test: 'test_en'
+          }
+        },
+        de: {
+          translation: {
+            test: 'test_de'
+          }
+        }
+      }))
+      await i18n.init()
+    })
+
+    const tests = [
+      {
+        args: [
+          'translation:test',
+          { appendNamespaceToCIMode: false, ns: 'translation', nsSeparator: ':' }
+        ],
+        expected: 'test'
+      },
+      {
+        args: ['test', { appendNamespaceToCIMode: false, ns: 'translation', nsSeparator: ':' }],
+        expected: 'test'
+      },
+      {
+        args: [
+          'translation:test',
+          { appendNamespaceToCIMode: true, ns: 'translation', nsSeparator: ':' }
+        ],
+        expected: 'translation:test'
+      },
+      {
+        args: ['test', { appendNamespaceToCIMode: true, ns: 'translation', nsSeparator: ':' }],
+        expected: 'translation:test'
+      }
+    ]
+
+    tests.forEach(test => {
+      it('correctly return key for ' + JSON.stringify(test.args) + ' args in cimode', () => {
+        should(i18n.t.apply(i18n, test.args)).eql(test.expected)
+      })
+    })
+  })
+})
---
+++ test/exists.spec.js
@@ -0,0 +1,46 @@
+import i18next from '../index.js'
+import should from 'should'
+
+describe('Translator', () => {
+  describe('exists()', () => {
+    let i18n
+
+    before(async () => {
+      i18n = i18next({ lng: 'en', fallbackLng: 'en' })
+      i18n.addHook('loadResources', () => ({
+        en: {
+          translation: {
+            test: 'test_en',
+            deep: {
+              test: 'deep_en'
+            }
+          }
+        },
+        de: {
+          translation: {
+            test: 'test_de'
+          }
+        }
+      }))
+      await i18n.init()
+    })
+
+    const tests = [
+      { args: ['translation:test'], expected: true },
+      { args: ['translation:test', { lngs: ['en-US', 'en'] }], expected: true },
+      { args: ['translation:test', { lngs: ['de'] }], expected: true },
+      { args: ['translation:test', { lng: 'de' }], expected: true },
+      { args: ['translation:test', { lng: 'fr' }], expected: true },
+      { args: ['translation:test', { lng: 'en-US' }], expected: true },
+      { args: ['translation.test', { lng: 'en-US', nsSeparator: '.' }], expected: true },
+      { args: ['translation.deep.test', { lng: 'en-US', nsSeparator: '.' }], expected: true },
+      { args: ['deep.test', { lng: 'en-US', nsSeparator: '.' }], expected: true }
+    ]
+
+    tests.forEach(test => {
+      it('correctly translates for ' + JSON.stringify(test.args) + ' args', () => {
+        should(i18n.exists.apply(i18n, test.args)).eql(test.expected)
+      })
+    })
+  })
+})
